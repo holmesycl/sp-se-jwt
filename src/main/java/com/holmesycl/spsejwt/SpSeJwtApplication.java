195baee7552e451ea37db1fd4565eeb5a0c498de
holmesycl <1047406364@qq.com>
2021-03-24 16:15:09 +0800

    项目初始化

---
+++ src/main/java/com/holmesycl/spsejwt/SpSeJwtApplication.java
@@ -0,0 +1,13 @@
+package com.holmesycl.spsejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpSeJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpSeJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/holmesycl/spsejwt/SpSeJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holmesycl.spsejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpSeJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
